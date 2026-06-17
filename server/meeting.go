eca740adb4067c2214686ad70b7156e7f1370172
Andre Vasconcelos <a-andre.vasconcelos@mattermost.com>
2026-06-17 15:54:06 +0300

    MM-69306 Improving meeting started messages to work on mobile (#16)
    * MM-69306 Improving meeting started messages to work on mobile
    
    * Changing format of message to match custom component's wording

--- server/meeting.go
+++ server/meeting.go
@@ -69,12 +69,23 @@ func (p *Plugin) StartMeeting(userID, channelID, topic, connectionID, rootPostID
 		message = fmt.Sprintf("I have started a meeting: **%s**", topic)
 	}
 
+	attachmentTitle := topic
+	if attachmentTitle == "" {
+		attachmentTitle = "Google Meet"
+	}
+	slackAttachment := model.SlackAttachment{
+		Fallback: fmt.Sprintf("Meeting started.\n\nMeeting URL: %s", meetURL),
+		Title:    attachmentTitle,
+		Text:     fmt.Sprintf("Meeting URL: %s", meetURL),
+	}
+
 	post := &model.Post{
 		UserId:    userID,
 		ChannelId: channelID,
 		Message:   message,
 		Type:      "custom_google_meet",
 		Props: model.StringInterface{
+			"attachments":   []*model.SlackAttachment{&slackAttachment},
 			"meeting_link":  meetURL,
 			"meeting_topic": topic,
 		},
